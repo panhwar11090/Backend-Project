bc56f159b541ac9861870b4fc73bc3770d4a01ac
Huzaifa Decotechs <127252478+panhwar11090@users.noreply.github.com>
2024-04-29 23:27:03 +0500

    access refresh token done

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,41 @@
 import multer from "multer";
+import path from "path";
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-})
+const storage = multer.diskStorage(
+  {
+      destination: function(req,file,cb){
+          cb(null, "./public/temp");
+      },
+      filename: function (req , file , cb){
+          cb(null, Date.now() + "-" + file.originalname);
+      }
+  }
+);
 
-export const upload = multer({
-    storage,
-})
+const fileFilter = (req , file, callback) => {
+  const validExts = [".png",".jpg", ".jpeg"];
+  if(!validExts.includes(path.extname(file.originalname))){
+      return callback(new Error("Only .png, .jpg and .jpeg format allowed"));
+  }
+  const fileSize = parseInt(req.headers["content-length"]);
+
+  if (fileSize > 1048576) {
+      return callback (new Error("File Size Big"));
+  }
+
+  callback(null, true);
+}
+
+let upload = multer({
+  storage: storage,
+  fileFilter:fileFilter,
+  fileSize:1048576
+});
+
+
+
+export  const uploadFields = upload.fields([
+  { name: "avatar", maxCount: 1 },
+  { name: "coverImage", maxCount: 1 }
+]);
   
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,27 +1,54 @@
-import { v2 as cloudinary } from "cloudinary";
-import fs from 'fs'
+// import { v2 as cloudinary } from "cloudinary";
+// import fs from 'fs'
 
-import {v2 as cloudinary} from 'cloudinary';
           
+// cloudinary.config({ 
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+//   api_key:process.env.CLOUDINARY_API_KEY, 
+//   api_secret:process.env.CLOUDINARY_API_SECRET  
+// });
+
+// const uploadCloudinary = async(localFilePath)=>{
+//     try {
+//         if(!localFilePath) return null
+//         //upload this file on cloudinary
+//         const response = await cloudinary.uploader.upload(localFilePath,{
+//             resource_type:"auto"
+//         })
+//         // console.log("file is uploaded on cloudinary", response.url);
+//         fs.unlinkSync(localFilePath)
+//         return response;
+//     } catch (error) {
+//         fs.unlinkSync(localFilePath)
+//         return null;
+//     }
+// }
+
+// export{uploadCloudinary}
+
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key:process.env.CLOUDINARY_API_KEY, 
-  api_secret:process.env.CLOUDINARY_API_SECRET  
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
-
-const uploadCloudinary = async(localFilePath)=>{
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath) return null
-        //upload this file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath,{
-            resource_type:"auto"
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
         })
-        console.log("file is uploaded on cloudinary", response.url);
-        return response;
-    } catch (error) {
+        // file has been uploaded successfull
+        console.log("file is uploaded on cloudinary ", response.url);
+        //console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
         return null;
     }
 }
-
-export{uploadCloudinary}
+export {uploadOnCloudinary}
